9127ed413769bdb6d360ff29ce239ad313f6364e
sjambudi <siddhi.jambudi@gmail.com>
2014-08-18 16:06:12 -0700

    change

--- M101J_2/src/main/java/com/tengen/DocumentRepresentationTest.java
+++ M101J_2/src/main/java/com/tengen/DocumentRepresentationTest.java
@@ -8,6 +8,7 @@
 
 package com.tengen;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import com.mongodb.BasicDBObject;
@@ -21,6 +22,14 @@ public class DocumentRepresentationTest
 		doc.put("birthDate", new Date(2343546));
 		doc.put("programmer", true);
 		doc.put("age", 45);
+		doc.put("languages", Arrays.asList("Java", "c++", "python"));
+		doc.put("address", new BasicDBObject("street", "50 main street").append("town", "fremont").append("zip", 94539));
+		System.out.println(doc);
+		
+		/*BasicDBObject quizDoc = new BasicDBObject();
+		quizDoc.put("_id", "user1");
+		quizDoc.put("Interests", Arrays.asList("basketball", "drumming"));
+		System.out.println(quizDoc);	*/	
 	}
 }
 
