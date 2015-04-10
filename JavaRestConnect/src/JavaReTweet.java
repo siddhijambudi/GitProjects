a5846c46688a87674601ec62af7f673456d0f9f3
siddhijambudi <prathit@gmail.com>
2015-04-09 17:17:30 -0700

    Update JavaReTweet.java

--- JavaRestConnect/src/JavaReTweet.java
+++ JavaRestConnect/src/JavaReTweet.java
@@ -5,10 +5,10 @@ import twitter4j.auth.AccessToken;
 
 public class JavaReTweet {
 
-	static String consumerKeyStr = "IHb4MXXbMOGExy89iF2IPO2lZ";
-	static String consumerSecretStr = "bNYXqMxWHCm7lGtu8twTuF9Xssq9AO4p4rBmsb8YBBLl5lq3R8";
-	static String accessTokenStr = "337385276-9n6GchOfSpYOeWsTOhIldKynhcJSJXM4Fz65Wj8F";
-	static String accessTokenSecretStr = "lY832Hh19WTZrxQbnoe8R5HQT2lLb9wjQ9Tl5RnpcnQIj";
+	static String consumerKeyStr = "XXXX";
+	static String consumerSecretStr = "XXXX";
+	static String accessTokenStr = "XXXX";
+	static String accessTokenSecretStr = "XXXXXX";
 
 	public static void main(String[] args) {
 
