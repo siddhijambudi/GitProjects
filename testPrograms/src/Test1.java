5d41548d0cf9af05c7b2cc6a1248c8e2d6a8584a
siddhijambudi <siddhi.jambudi@gmail.com>
2019-01-12 14:59:21 -0800

    recursion practice

--- testPrograms/src/Test1.java
+++ testPrograms/src/Test1.java
@@ -1,5 +1,19 @@
 public class Test1 {
     public static void main(String args[]){
-        System.out.println("Test 112");
+        System.out.println(strCopies("catcowcat", "cow", 1));
+    }
+    static int number = 0;
+    public static boolean strCopies(String str, String sub, int n) {
+        if(str.length() == 0 || str.length() < sub.length()){
+            if(number == n){
+                return true;
+            }
+            return false;
+        }
+        if(str.substring(0, sub.length()).equals(sub)){
+            number = number + 1;
+            return strCopies(str.substring(1) , sub, n);
+        }
+        return strCopies(str.substring(1), sub, n);
     }
 }
