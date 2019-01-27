7900b5c864bc6430b3f2a020bb8816452ab9f896
siddhijambudi <siddhi.jambudi@gmail.com>
2019-01-26 19:42:48 -0800

    second_version_subset_complete

--- testPrograms/src/GenerateAllExpressions.java
+++ testPrograms/src/GenerateAllExpressions.java
@@ -1,30 +1,48 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GenerateAllExpressions {
+    static int countLeaf = 1;
     public static void main(String args[]){
-        List<String> result = new ArrayList<>();
-        generateAllExpressions(0,"",0, "123", 6, result);
+        Set<String> result = new HashSet<>();
+
+
+        generateAllExpressions(0,"",0, 0, "1234", 11, result);
 
         //print result
-        for(String resString : result){
-            System.out.println(resString);
+        int k = 0;
+        String[] strArray = new String[result.size()];
+        for(Object str : result.toArray()){
+            strArray[k] = (String) str;
+            k++;
         }
+        for(String s : strArray){
+            //System.out.println(s);
+        }
+        System.out.println("Total leaves: " + countLeaf);
     }
     public static void generateAllExpressions(int arrayIndex, String holdStringExprSofar, int holdCurrValuesForStrExprSoFar,
-                                                       String inputString, int target, List<String> result){
+                                                       int last, String inputString, int target, Set<String> result){
 
         //when a leaf node is holding value same as target add it into result array
-        if(holdCurrValuesForStrExprSoFar == target){
+        /*if(holdCurrValuesForStrExprSoFar == target){
             result.add(holdStringExprSofar);
+            return;
+        }*/
+        if(arrayIndex == inputString.length()){
+            System.out.println("leaf : " + holdStringExprSofar + " leaf count: " + countLeaf + " value leaf is holding " + holdCurrValuesForStrExprSoFar + " with last value: " + last);
+            countLeaf ++;
+            System.out.println("return 1");
             return;
         }
 
         //loop through each index
-        for(int i = arrayIndex; i < inputString.length(); i++){
+        //int i = arrayIndex;
+        //while( i < inputString.length()){
+
+            //System.out.println(holdStringExprSofar + " at " + " i: " + i + " array index : " + arrayIndex);
 
             //check what is the string so far
-            String currString = inputString.substring(arrayIndex, i + 1);
+            String currString = String.valueOf(inputString.charAt(arrayIndex));//inputString.substring(arrayIndex, i + 1);
 
             //check what is the value so far
             int currValue = Integer.valueOf(currString);
@@ -33,20 +51,67 @@ public class GenerateAllExpressions {
             //go to the deep of first character of the input string
             if(arrayIndex == 0){
                 generateAllExpressions(arrayIndex + 1, holdStringExprSofar + currString,
-                        currValue, inputString, target, result);
+                        currValue, currValue, inputString, target, result);
             }
 
             //next index check with all possible values
             else{
                 generateAllExpressions(arrayIndex + 1, holdStringExprSofar + "" + currString,
-                        Integer.valueOf(inputString.substring(0, i + 1)), inputString, target, result);
+                        holdCurrValuesForStrExprSoFar, currValue, inputString, target, result);
 
                 generateAllExpressions(arrayIndex + 1, holdStringExprSofar + "*" + currString,
-                        holdCurrValuesForStrExprSoFar * currValue, inputString, target, result);
+                        holdCurrValuesForStrExprSoFar - last + last * currValue, last * currValue, inputString, target, result);
 
                 generateAllExpressions(arrayIndex + 1, holdStringExprSofar + "+" + currString,
-                        holdCurrValuesForStrExprSoFar + currValue, inputString, target, result);
+                        holdCurrValuesForStrExprSoFar + currValue, currValue, inputString, target, result);
+
+                return;
             }
-        }
+            //i++;
+        //}
+        //System.out.println("return 3: explicit " + " value of i: " + i);
     }
+    /*static Integer convertExpression(String str){
+
+        int multiPlayIndex = str.indexOf('*'); //3
+        int additionIndex = str.indexOf('+'); //1
+
+        if(multiPlayIndex == -1 && additionIndex == -1){
+            return null;
+        }
+        Integer mul = null;
+        Integer add = null;
+
+        if(multiPlayIndex != -1){
+            mul = 1;
+        }
+        if(additionIndex != -1){
+            add = 0;
+        }
+        Integer mcount = null;
+        String[] mulArray;
+        if(mul != null) {
+           mcount = 1;
+           mulArray = str.split("\\*");
+           for(String m : mulArray){
+              mcount = mcount * Integer.valueOf(m);
+           }
+        }
+        String[] addArray;
+        Integer addCount = null;
+        if(add != null) {
+           addCount = 0;
+           addArray = str.split("\\+");
+           for(String m1 : addArray){
+             addCount = addCount + Integer.valueOf(m1);
+           }
+        }
+        if(mcount == null){
+            return addCount;
+        }
+        if(addCount == null){
+            return mcount;
+        }
+        return mcount + addCount;
+    }*/
 }
